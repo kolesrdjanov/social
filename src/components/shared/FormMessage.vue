7589861a5437ec4c43cb6a1a4a7c705e50243fe8
Konstantin Srdjanov <konstantinsrdjanov@Konstantins-MacBook-Air.local>
2023-03-27 20:28:12 +0200

    chore: revert component

---
+++ src/components/shared/FormMessage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="form-message rounded fw-500"
+    :class="`form-message--${type}`">
+      {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+div.form-message {
+  font-size: 12px;
+  font-weight: 400;
+
+  &.form-message--success {
+    color: $color-success;
+  }
+
+  &.form-message--error {
+    color: $color-error;
+  }
+}
+</style>
